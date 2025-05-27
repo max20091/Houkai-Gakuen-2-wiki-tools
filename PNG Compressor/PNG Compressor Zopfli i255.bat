ae38c274ca8cb16fc922eda4f6be51931c457394
What? <boostyourprogram@gmail.com>
2025-05-28 00:24:32 +0700

    New PNG compression method
    Taking advantage of oxipng parallel compression.

--- PNG Compressor/PNG Compressor Zopfli i255.bat
+++ PNG Compressor/PNG Compressor Zopfli i255.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Compress PNG
 if not exist "%CD%\file\" mkdir "%CD%\file\"
-oxipng -o max --strip safe -r "%CD%\file"
+oxipng -o max -Z --zi 255 --strip safe -r "%CD%\file"
 pause
 exit
